e88bf010a8cb163ab5374c9afeaf41cd92ff8c3a
danelegassick <danelegassick@gmail.com>
2022-10-27 20:19:16 -0400

    BIG SITE CHANGES + 2 PORTFOLIO
    JS ADDED, PAGES UPDATED, BIG BIG BIG

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Scroll Indicator Progress Bar
+ */
+ const handleScrollIndicator = () => {
+	const scrollIndicator = document.querySelector("#scroll-indicator");
+	const maxHeight = document.body.scrollHeight - window.innerHeight;
+
+	const widthPercentage = (window.scrollY / maxHeight) * 100;
+	scrollIndicator.style.width = `${widthPercentage}%`;
+};
+
+window.addEventListener("scroll", handleScrollIndicator);
